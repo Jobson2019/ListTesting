0575b8c54911e52c4fa2f61ad4970673301a578b
Kevin Jobson <jobsonk2877@gmail.com>
2019-10-09 13:28:44 -0500

    Add function completed

--- TestingProject/TestingProject/CustomList.cs
+++ TestingProject/TestingProject/CustomList.cs
@@ -6,7 +6,95 @@ using System.Threading.Tasks;
 
 namespace TestingProject
 {
-    class CustomList
+    public class CustomList<T>
     {
+            
+
+        public T this [int index]
+        {
+            get
+            {
+                T temp;
+                if (index >= 0 && index <= count -1)
+                {
+                    temp = items[index];
+                }
+                else
+                {
+                    throw new System.ArgumentException("Index out of range");
+                }
+                return (temp);
+            }
+            set
+            {
+                if (index >= 0 && index <= capacity -1)
+                {
+                    items[index] = value;
+                }
+            }
+        }
+
+        public void Add(T itemToAdd)
+        {
+            if (count == capacity)
+            {
+                GrowArray();
+            }
+            items[count] = itemToAdd;
+            IncrementCount();
+        }
+
+        private T[] items;
+            
+
+            public CustomList ()
+            {
+                items = new T[4];
+            }
+            public void Add(T itemToAdd)
+            {
+             
+            
+                 
+                
+                    items[count] = itemToAdd;
+                    IncrementCount();
+                
+            
+            }
+        private int count;
+        private int capacity;
+
+        public void GrowArray()
+        {
+
+        }
+        
+        public int Count
+        {
+            get
+            {
+                return count;
+            }
+        }
+        public int Capacity
+        {
+           get
+            {
+                return capacity;
+            }
+        }
+
+            public void Clear()
+            { 
+            }
+        public void IncrementCount()
+        {
+            count += 1;
+        }
+
+
+
+        
     }
 }
